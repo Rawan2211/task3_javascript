dd1b4b0f945e6733a33023acbfb51650a870a119
Rawan2211 <96665139+Rawan2211@users.noreply.github.com>
2023-09-20 12:21:16 +0300

    Update product.js

--- product.js
+++ product.js
@@ -3,34 +3,41 @@
         var element = document.body;
         element.classList.toggle("dark-mode");
     };
-    
+    const keysValues = window.location.search;
+    const urlParams = new URLSearchParams(keysValues);
+    const param = urlParams.get('id');
+
     function productFetchfunction(){
     fetch("https://dummyjson.com/products/1").then((result) => {
     let myData = result.json();
-    console.log(myData);
     return myData;
     
+    }).then((myData) => {
+        let res=  myData.products;
+        res.length=28;
+        return res;
     }).then((values) =>{
-       // let data="";
+       for (i = 0 ; i<=param ; i++){
+        for (j=0 ; j<i; j++){
         data = `<div class="card">
-            <img src=${values.images[0]} alt="" class="images img-fluid">
-            <h3 class="title"> ${values.title} </h3>
-            <div class = "description" id="desc"><span class="fw-bold">Description:</span> ${values.description} </div>
-            <p class="price"><span class="fw-bold">Price:</span> ${values.price}</p> 
-            <p class="discount"><span class="fw-bold">Discount:</span> ${values.discountPercentage}</p> 
-            <p class="rating"><span class="fw-bold">Rating:</span> ${values.rating}</p> 
-            <p class="brand"><span class="fw-bold">Brand:</span> ${values.brand}</p> 
-            <p class="category"><span class="fw-bold">Category:</span> ${values.category}</p>
-            <p class="stock"><span class="fw-bold">In stock!:</span> ${values.stock}</p> 
+            <img src=${values[j].images[0]} alt="" class="images img-fluid">
+            <h3 class="title"> ${values[j].title} </h3>
+            <div class = "description" id="desc"><span class="fw-bold">Description:</span> ${values[j].description} </div>
+            <p class="price"><span class="fw-bold">Price:</span> ${values[j].price}</p> 
+            <p class="discount"><span class="fw-bold">Discount:</span> ${values[j].discountPercentage}</p> 
+            <p class="rating"><span class="fw-bold">Rating:</span> ${values[j].rating}</p> 
+            <p class="brand"><span class="fw-bold">Brand:</span> ${values[j].brand}</p> 
+            <p class="category"><span class="fw-bold">Category:</span> ${values[j].category}</p>
+            <p class="stock"><span class="fw-bold">In stock!:</span> ${values[j].stock}</p> 
  
 
         </div>`
 
         document.getElementById("card").innerHTML=data;
-    
-    }
-    ).catch((err) =>{
-    console.log(err);
+        }
+       }
+    }).catch((err) =>{
+
     });
 }
 productFetchfunction();
