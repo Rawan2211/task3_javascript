69590d7026ea425324d4e91f676baaf6808ffa23
Rawan2211 <96665139+Rawan2211@users.noreply.github.com>
2023-09-20 14:47:20 +0300

    Update product.js

--- product.js
+++ product.js
@@ -37,6 +37,7 @@
         </div>`
 
         document.getElementById("card").innerHTML=data;
+        document.getElementById("mobileCard").innerHTML=data;
         }
 
     }
@@ -48,41 +49,4 @@
 productFetchfunction();
 
 
-
-function mobileProductFetch(){
-    fetch("https://dummyjson.com/products").then((result) => {
-    let myData = result.json();
-    return myData;
-    
-    }).then((myData) => {
-        let res=  myData.products;
-        res.length=28;
-        return res;
-}).then((values) =>{
-
-    for (i = 0 ; i<=param ; i++){
-        for (j=0 ; j<i; j++){
-        data = `<div class="card">
-        <img src=${values[j].images[0]} alt="" class="images img-fluid">             
-        <h3 class="title"> ${values[j].title} </h3>
-        <div class = "description" id="desc"><span class="fw-bold">Description:</span> ${values[j].description} </div>
-        <p class="price"><span class="fw-bold">Price:</span> ${values[j].price}</p> 
-        <p class="discount"><span class="fw-bold">Discount:</span> ${values[j].discountPercentage}</p> 
-        <p class="rating"><span class="fw-bold">Rating:</span> ${values[j].rating}</p> 
-        <p class="brand"><span class="fw-bold">Brand:</span> ${values[j].brand}</p> 
-        <p class="category"><span class="fw-bold">Category:</span> ${values[j].category}</p>
-        <p class="stock"><span class="fw-bold">In stock!:</span> ${values[j].stock}</p> 
-
-        </div>`
-
-        document.getElementById("mobileCard").innerHTML=data;
-        }
-
-    }
-
-    }).catch((err) =>{
-
-    });
-}
-mobileProductFetch();
 
